6cf5f2cf3f1c0f7df7753ec6964c46394b2cee10
Darren W <darrenwallace@gmail.com>
2024-09-21 15:01:21 -0400

    added launch file and changed to thrustmaster joystick

--- install/DW_DarrensController/share/DW_DarrensController/package.ps1
+++ install/DW_DarrensController/share/DW_DarrensController/package.ps1
@@ -112,5 +112,6 @@ $env:COLCON_CURRENT_PREFIX=(Get-Item $PSCommandPath).Directory.Parent.Parent.Ful
 
 colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/DW_DarrensController/hook/pythonpath.ps1"
 colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\share/DW_DarrensController/hook/ament_prefix_path.ps1"
+colcon_package_source_powershell_script "$env:COLCON_CURRENT_PREFIX\../../build/DW_DarrensController/share/DW_DarrensController/hook/pythonpath_develop.ps1"
 
 Remove-Item Env:\COLCON_CURRENT_PREFIX
--- install/DW_DarrensController/share/DW_DarrensController/package.sh
+++ install/DW_DarrensController/share/DW_DarrensController/package.sh
@@ -80,6 +80,7 @@ _colcon_package_sh_source_script() {
 # source sh hooks
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/DW_DarrensController/hook/pythonpath.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/DW_DarrensController/hook/ament_prefix_path.sh"
+_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/../../build/DW_DarrensController/share/DW_DarrensController/hook/pythonpath_develop.sh"
 
 unset _colcon_package_sh_source_script
 unset COLCON_CURRENT_PREFIX
