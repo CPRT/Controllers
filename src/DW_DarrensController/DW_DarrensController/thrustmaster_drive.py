a9b7a2814fe609067d6c414015b8d739ea8bd898
Darren W <darrenwallace@gmail.com>
2024-09-22 20:08:23 -0400

    subscribe to Joy node working

--- src/DW_DarrensController/DW_DarrensController/thrustmaster_drive.py
+++ src/DW_DarrensController/DW_DarrensController/thrustmaster_drive.py
@@ -6,19 +6,26 @@ from math import radians
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
 
+MAX_SPEED = 2.0     #2 m/s
+MAX_ACC = 
+MIN_ACC = 
+
 class thrustmaster_drive(Node):
     def __init__(self):
         super().__init__("thrustMasterDrive")
         self.twist = Twist()
         
-        self.cmd_move_subscriber = self.create_subscription(Joy, "joystick", self.cmd_joy_callback, 10)
+        self.setTwistPub = self.create_publisher(Twist, "TM_Pub", 1)
+        self.cmd_move_subscriber = self.create_subscription(Joy, "joy", self.joy_callback, 10)
 
-        self.setTwistPub = self.create_publisher(Twist, "Joystick_pub", 1)
         
-    def cmd_joy_callback(self, msg: Joy):
-        #self.twist.linear.x = #axis
+    def joy_callback(self, msg: Joy):
+        
+        
+        self.twist.linear.x = msg.axes[1]
+        self.twist.linear.z = msg.axes[0]
         self.setTwistPub.publish(self.twist)
-
+        
 
 def main(args=None):
     rclpy.init(args=args)
