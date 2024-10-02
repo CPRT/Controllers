d47495b0a4efeb7afdf10b6d7cce36e9d8a5a396
Will Richards <will.f.richards@gmail.com>
2024-10-02 18:24:45 -0400

    reajusted the joystick values, initialized ROSLIB and added roswebridge

---
+++ src/main.js
@@ -0,0 +1,30 @@
+const canvas = document.createElement('canvas'), context = canvas.getContext('2d');
+document.body.append(canvas);
+
+let width = canvas.width = innerWidth;
+let height = canvas.height = innerHeight;
+
+const FPS = 120;
+
+function background() {
+    context.fillStyle = '#000';
+    context.fillRect(0, 0, width, height);
+}
+
+let joysticks = [
+    new Joystick(width/2, height/2, width/6, width/12),
+    //new Joystick(width - 80, height - 80, 50, 25)
+];
+
+setInterval(() => {
+    background();
+
+    for (let joystick of joysticks) {
+        joystick.update();
+    }
+
+    
+
+    
+
+}, 1000 / FPS);
