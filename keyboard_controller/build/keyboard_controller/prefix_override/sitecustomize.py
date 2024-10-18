51ec89666d3da1745c29f4a796fda5f1a2cd7170
Jayden442 <146302029+Jayden442@users.noreply.github.com>
2024-10-18 15:15:48 -0400

    Oops

--- keyboard_controller/build/keyboard_controller/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/jayden/ros2_realController_ws/src/keyboard_controller/install/keyboard_controller'
